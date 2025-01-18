794f3aee9becc3d083a011936e16d410cbacd850
Liruvien <liruvien@gmail.com>
2025-01-18 16:37:47 +0100

    added main files and directories

--- README.md
+++
@@ -1,1 +0,0 @@
-# Perdiod-tracker-app
---
+++ period_tracker_project/period_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PeriodAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'period_app'
